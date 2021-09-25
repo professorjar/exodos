12b0d95b5224db2613570c8768382620a07f2173
professorjar <66971871+professorjar@users.noreply.github.com>
2021-09-24 22:12:39 -0300

    GRUD
    GRUD

--- __init__.py
+++ __init__.py
@@ -9,11 +9,12 @@ class Bvsmatrix:
         self.indicado=i
     def mostrar(self):
         print('Professor: JOSÉ ALBERTO RODRIGUES VERSÃO 1.0.3 24/09/2021:')
-        print('Projetos no site do ava & sae..: https://github.com/professorjar')
-        print('Funcionário....................: ' + str(self.funcionario))
+        print('Projetos CRUD no site do ava & sae ..: https://github.com/professorjar')
+        print('C..: INSERT R..:Read U..:UPDATE   D..: DELETE')
+        print('Funcionário..........................: ' + str(self.funcionario))
         estado = 'sim' if self.trabalhando else 'Não'
-        print('Trabalhando....................: ' + estado)
-        print('Indicado.......................: ' + (self.indicado))
+        print('Trabalhando..........................: ' + estado)
+        print('Indicado.............................: ' + (self.indicado))
         print('Projetos..: https://professorjar.github.io/gerenciamentodeprojetos/index.html')
         print('=' *100)
 fu1=Bvsmatrix(778,False,'Sim,por Morpheus')
